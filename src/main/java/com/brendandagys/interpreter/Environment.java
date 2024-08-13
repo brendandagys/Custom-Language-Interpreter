af31b55d6da509748b3cf123d6138ce76b49f89a
Brendan Dagys <brendandagys@gmail.com>
2024-08-13 19:28:26 -0400

    Implement assignment in parser and interpreter
    Update grammar to reflect changes.
    Create new `Assign` class that extends `Expr`.

--- src/main/java/com/brendandagys/interpreter/Environment.java
+++ src/main/java/com/brendandagys/interpreter/Environment.java
@@ -11,7 +11,16 @@ class Environment {
       return values.get(name.lexeme);
     }
 
-    throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+    throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'");
+  }
+
+  void assign(Token name, Object value) {
+    if (values.containsKey(name.lexeme)) {
+      values.put(name.lexeme, value);
+      return;
+    }
+
+    throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'");
   }
 
   void define(String name, Object value) {
