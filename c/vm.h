534a897da6166a4a096e40d778e9a98a388fa8ce
Brendan Dagys <brendandagys@gmail.com>
2024-08-30 15:15:21 -0400

    Call frames

--- c/vm.h
+++ c/vm.h
@@ -1,15 +1,24 @@
 #ifndef compiler_vm_h
 #define compiler_vm_h
 
-#include "chunk.h"
+#include "object.h"
 #include "table.h"
 #include "value.h"
 
-#define STACK_MAX 256
+#define FRAMES_MAX 64
+#define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
+
+// Represents a single ongoing function call
+typedef struct {
+  ObjFunction* function;  // Function being called
+  uint8_t* ip;            // Jump here when function returns
+  Value* slots;           // Points to first slot that function can use
+} CallFrame;
 
 typedef struct {
-  Chunk* chunk;
-  uint8_t* ip;
+  CallFrame frames[FRAMES_MAX];
+  int frameCount;  // Current height of `CallFrame` stack
+
   Value stack[STACK_MAX];
   Value* stackTop;
   Table globals;
